5e3568b1b9c2278e8c1b7434fb54704417b692d2
BazettFraga <holydaggertm@gmail.com>
2026-04-13 20:33:05 +0200

    properly draw & center the piece

--- src/draw.c
+++ src/draw.c
@@ -21,6 +21,8 @@ enum {  CORNER_DISPLAY_TOP_LEFT,
 
 //warning! every function is responsible for setting rect's attributes on its own!!!!!
 SDL_FRect rect;
+
+//FIXME: holy shit. clean this up.
 const int CELL_SIZE = WINDOW_WIDTH > WINDOW_HEIGHT ? WINDOW_HEIGHT/BOARD_HEIGHT : WINDOW_WIDTH/BOARD_WIDTH;
 const float GAME_BOARD_WIDTH = CELL_SIZE*BOARD_WIDTH;
 const int game_board_clear_size = (WINDOW_WIDTH-GAME_BOARD_WIDTH)/2; //FIXME: sane name needed
@@ -30,6 +32,9 @@ const int corner_display_w = game_board_clear_size-(corner_display_w_padding*2);
 const int corner_display_h = WINDOW_HEIGHT*CORNER_DISPLAY_HEIGHT_FRACTION;
 const int corner_display_title_height = corner_display_w*CORNER_DISPLAY_TITLE_FRACTION;
 int start_pos = (WINDOW_WIDTH/2)-(BOARD_WIDTH/2)*CELL_SIZE;
+
+static CornerDisplay next_tetromino_corner_display;
+static CornerDisplay held_tetromino_corner_display;
 
 void debug_gravity(GameState *gamestate){
     char debug_text[64];
@@ -45,10 +50,9 @@ void draw_game(GameState *gamestate){
     draw_board(gamestate->board, gamestate->renderer);
     draw_player(&gamestate->player, gamestate->renderer);
     draw_player_shadow(&gamestate->player, gamestate->board, gamestate->renderer);
-    draw_corner_display(gamestate->renderer, "placeholder", CORNER_DISPLAY_TOP_LEFT);
-    draw_corner_display(gamestate->renderer, "placeholder", CORNER_DISPLAY_BOTTOM_LEFT);
-    draw_corner_display(gamestate->renderer, "placeholder", CORNER_DISPLAY_TOP_RIGHT);
-    draw_corner_display(gamestate->renderer, "placeholder", CORNER_DISPLAY_BOTTOM_RIGHT);
+    calculate_corner_display(&next_tetromino_corner_display, CORNER_DISPLAY_TOP_RIGHT, "");
+    draw_corner_display(gamestate->renderer, &next_tetromino_corner_display);
+    draw_tetromino_in_corner_display(gamestate->renderer, &next_tetromino_corner_display, &gamestate->piece_data[T_I]);
     debug_gravity(gamestate);
     SDL_RenderPresent(gamestate->renderer);
 }
@@ -56,8 +60,8 @@ void draw_game(GameState *gamestate){
 void draw_pause(GameState *gamestate){
     SDL_SetRenderDrawColor(gamestate->renderer, 64,192,192,128);
     SDL_FRect some_rect = {
-        .x = 10,
-        .y = 10,
+        .x = (WINDOW_WIDTH/2.0f)-110,
+        .y = (WINDOW_HEIGHT/2.0f)-230,
         .w = 220,
         .h = 460,
     };
@@ -66,21 +70,72 @@ void draw_pause(GameState *gamestate){
     SDL_RenderPresent(gamestate->renderer);
 }
 
-void draw_corner_display(SDL_Renderer *renderer, char *title, int pos){
-    rect.w = corner_display_w;
-    rect.h = corner_display_h;
+void draw_tetromino_in_corner_display(SDL_Renderer *renderer, CornerDisplay *display, PieceData *tetromino){
+    int cell_size = display->h/6; //assume our height will always be smaller than the width
+    int t_min_w, t_max_w, t_min_h, t_max_h;
+    int i;
+
+    t_max_w = t_min_w = tetromino->offset[0][0].x;
+    t_max_h = t_min_h = tetromino->offset[0][0].y;
+
+    rect.w = rect.h = cell_size;
+    SDL_Color color = tetromino->color;
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+
+    for(i=1; 4 > i; ++i){
+        int x = tetromino->offset[0][i].x;
+        int y = tetromino->offset[0][i].y;
+        t_max_w = x > t_max_w ? x : t_max_w;
+        t_min_w = x < t_min_w ? x : t_min_w;
+        t_max_h = y > t_max_h ? y : t_max_h;
+        t_min_h = y < t_min_h ? y : t_min_h;
+    }
+
+    int t_width = t_max_w - t_min_w + 1;
+    int t_height = t_max_h - t_min_h + 1;
+    int center_x, center_y;
+    get_corner_display_center(display, &center_x, &center_y);
+    int start_x = center_x-(t_width*cell_size/2.0f);
+    int start_y = center_y-(t_height*cell_size/2.0f);
+
+    for(i=0; 4 > i; ++i){
+        rect.x = SDL_abs(t_min_w*cell_size)+start_x+(tetromino->offset[0][i].x*cell_size);
+        rect.y = SDL_abs(t_min_h*cell_size)+start_y+(tetromino->offset[0][i].y*cell_size);
+        SDL_RenderFillRect(renderer,&rect);
+    }
+}
+
+void get_corner_display_center(CornerDisplay *display, int *x, int *y){
+    if(x)
+        *x = display->x+(display->w/2);
+    if(y)
+        *y = (display->y+display->title_h)+((display->h-display->title_h)/2);
+}
+
+void calculate_corner_display(CornerDisplay *display, int pos, char *title){
+    display->w = corner_display_w;
+    display->h = corner_display_h;
+    display->x = SDL_abs(((WINDOW_WIDTH-corner_display_w)*(bool)(pos & CORNER_DISPLAY_RIGHT)) - corner_display_w_padding);
+    display->y = SDL_abs(((WINDOW_HEIGHT-corner_display_h)*(bool)(pos & CORNER_DISPLAY_BOTTOM)) - corner_display_h_padding);
+    display->title_h = corner_display_title_height;
+}
+
+void draw_corner_display(SDL_Renderer *renderer, CornerDisplay *display){
+    rect.w = display->w;
+    rect.h = display->h;
 
     //TODO: evaulate if these operations are worth what we save in eliminating branches
     //FIXME: we cast the results to bools so any >1 value becomes 1. find cleaner way to do this?
-    rect.x = SDL_abs(((WINDOW_WIDTH-corner_display_w)*(bool)(pos & CORNER_DISPLAY_RIGHT)) - corner_display_w_padding);
-    rect.y = SDL_abs(((WINDOW_HEIGHT-corner_display_h)*(bool)(pos & CORNER_DISPLAY_BOTTOM)) - corner_display_h_padding);
+    rect.x = display->x;
+    rect.y = display->y;
   
     SDL_SetRenderDrawColor(renderer, 16, 192, 16, SDL_ALPHA_OPAQUE);  
     SDL_RenderFillRect(renderer, &rect);
     SDL_SetRenderDrawColor(renderer, 16, 96, 16, SDL_ALPHA_OPAQUE);  
-    rect.h = corner_display_title_height;
+    rect.h = display->title_h;
     SDL_RenderFillRect(renderer, &rect);
 }
+
 
 void draw_board(unsigned char (*board)[10], SDL_Renderer *renderer)
 {
